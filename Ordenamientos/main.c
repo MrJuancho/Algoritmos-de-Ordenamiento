b502457959b5a104c5607119339d445e9e4eca0e
Carlos Cata <carlos_cata27@hotmail.com>
2018-10-25 00:09:22 -0500

    cola
    Co-Authored-By: Carlos Catarino <carloskta27@users.noreply.github.com>

--- Ordenamientos/main.c
+++ Ordenamientos/main.c
@@ -13,9 +13,7 @@ double performancecounter_diff(LARGE_INTEGER *a, LARGE_INTEGER *b)
 int main()
 {
     clock_t start = clock();
-    printf("Hola cara de bola te apesta la cola\n");
-    printf("Tiempo transcurrido: %f", ((double)clock() - start) / CLOCKS_PER_SEC);
+        printf("Tiempo transcurrido: %f", ((double)clock() - start) / CLOCKS_PER_SEC);
 
     return 0;
 }
-*/
