dea2c39bebaa505c8875edbc62832514b0859283
Juan Carlos <juancho131401@gmail.com>
2018-10-25 00:20:38 -0500

    menu

--- Ordenamientos/main.c
+++ Ordenamientos/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
+#include "funciones.h"
 
 double performancecounter_diff(LARGE_INTEGER *a, LARGE_INTEGER *b)
 {
@@ -20,7 +21,8 @@ int main()
     {
         arreglo[i] = 1+ rand() % ((150000+1)-1);
     }
-
+    char m;
+    menu_i();
     clock_t start = clock();
     printf("Tiempo transcurrido: %f", ((double)clock() - start) / CLOCKS_PER_SEC);
 
