58159e73d7c428224ccb4b78cbde535d9f758ed8
Juan Carlos <juancho131401@gmail.com>
2018-10-25 00:17:10 -0500

    menu

---
+++ Ordenamientos/funciones.h
@@ -0,0 +1,21 @@
+//
+// Created by MrJua on 25/10/2018.
+//
+
+#ifndef ORDENAMIENTOS_FUNCIONES_H
+#define ORDENAMIENTOS_FUNCIONES_H
+
+#include <stdio.h>
+
+void menu_i(){
+    printf("1 >> Ordenamiento Burbuja.\n"
+           "2 >> Ordenamiento por Seleccion.\n"
+           "3 >> Ordenamiento por Inserccion.\n"
+           "4 >> Ordenamiento QuickSort.\n"
+           "5 >> Ordenamiento Shell.\n"
+           "0 >> Salir.");
+}
+
+
+
+#endif //ORDENAMIENTOS_FUNCIONES_H
