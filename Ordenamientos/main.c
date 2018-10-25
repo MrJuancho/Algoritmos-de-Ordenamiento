72bf49aa87742d93a85e1834fb030a51f29f6804
Carlos Cata <carlos_cata27@hotmail.com>
2018-10-24 23:57:07 -0500

    Update Ordenamientos/main.c
    Metodos de Ordenamiento

--- Ordenamientos/main.c
+++ Ordenamientos/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
 
-int main() {
-    printf("-A L G O R I T M O S   D E    O R D E N A M I E N T O-");
+int main()
+{
+    printf("-A L G O R I T M O S   D E    O R D E N A M I E N T O-\n");
+    printf("huevos para juan ja ja\n");
     return 0;
 }
