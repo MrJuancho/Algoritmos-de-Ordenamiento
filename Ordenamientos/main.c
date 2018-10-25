558fb638d9151033b54c8f425351e0fbaaf4db67
Juan Carlos <juancho131401@gmail.com>
2018-10-25 00:08:00 -0500

    COdigo sin comments

--- Ordenamientos/main.c
+++ Ordenamientos/main.c
@@ -3,19 +3,6 @@
 #include <time.h>
 #include <windows.h>
 
-int main()
-{
-    printf("-A L G O R I T M O S   D E    O R D E N A M I E N T O-\n");
-
-
-    return 0;
-}
-
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <windows.h>
-
 double performancecounter_diff(LARGE_INTEGER *a, LARGE_INTEGER *b)
 {
   LARGE_INTEGER freq;
